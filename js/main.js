5bcd97f93194b1b034de7698c2ccfd747dca0364
Juan Alzate <juan.f1894@gmail.com>
2023-09-01 20:29:29 -0500

    Se actualiza el código de la resta

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ function restar() {
     let numUno = document.getElementById("varUno").value;
     let numDos = document.getElementById("varDos").value;
     let resultado = parseInt(numUno) - parseInt(numDos);
-    document.getElementById("resultado").innerText = resultado;
+    document.getElementById("resultadoResta").innerText = resultado;
 }
 
 function sumar() {
     let numUno = document.getElementById("numUno").value;
     let numDos = document.getElementById("numDos").value;
     let resultado = parseInt(numUno) + parseInt(numDos);
-    document.getElementById("resultado").innerText = resultado;
+    document.getElementById("resultadoSuma").innerText = resultado;
     console.log(resultado);
 }
