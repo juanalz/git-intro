3bbcb15eca1c8129afcb779ae3f8dbc0f564e551
Stivenjimenez08 <117239706+Stivenjimenez08@users.noreply.github.com>
2023-09-01 19:59:47 -0500

    Se retiran los console.log de la funcion restar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 function restar() {
     let numUno = document.getElementById("varUno").value;
     let numDos = document.getElementById("varDos").value;
-    console.log(numUno)
-    console.log(numDos)
     let resultado = parseInt(numUno) - parseInt(numDos);
     document.getElementById("resultado").innerText = resultado;
-    console.log(resultado);
 }
